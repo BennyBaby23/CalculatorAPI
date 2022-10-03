331e8677f761e5c5faebf8532c69247672731d15
ReejaMaryMathew <114038169+ReejaMaryMathew@users.noreply.github.com>
2022-10-02 20:42:05 -0400

    first commit

---
+++ app/src/main/java/com/mdev/calculatorapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mdev.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*File name: CalculatorAPP
+Author Name: Benny Baby
+STUDENT ID : 200469127
+App Description : Unique calculator APP for calculation
+Version: Android Studio Dolphin | 2021.3.1 for Windows 64-bit */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
